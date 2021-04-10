79292da3c3e1f053a6454f752de6b24dfaf9823e
Pedro Tashima <pedrotashima42@gmail.com>
2021-04-10 20:55:57 -0300

    style(server): added git repo to page body

--- src/utils/server.js
+++ src/utils/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 export default function () {
   return new Koa()
     .use(async ctx => {
-      ctx.body = ':)'
+      ctx.body = '<a href=https://github.com/Tashima42/lorem-ipsum-discord>Git repository</a>'
     })
     .listen(port)
 }
